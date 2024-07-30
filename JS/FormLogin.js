d2efe3d59945ec89a828e0c2a7f383aae4cc4694
9Nadi_ <marrief19@gmail.com>
2024-07-31 05:01:41 +0700

    add registration page

--- JS/FormLogin.js
+++ JS/FormLogin.js
@@ -2,21 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
   // ------------------------------//
   //  DOM Form Login               //
   // ------------------------------//
-  const formLoginContainer = document.getElementById("FormLoginJS");
-  const loginForm = createLoginForm();
-  formLoginContainer.appendChild(loginForm);
+  const FormLogin = document.getElementById("FormLoginJS");
+  const LoginForm = createLoginForm();
+  FormLogin.appendChild(LoginForm);
   // ------------------------------//
   //  END DOM From Login            //
   // ------------------------------//
-});
+});  
 
 // ------------------------------//
 //  From Login Structure         //
 // ------------------------------//
 function createLoginForm() {
-  const form = document.createElement("form");
-  form.className = "login-form";
-  form.innerHTML = `
+  const FormLog = document.createElement("form");
+  FormLog.className = "login-form";
+  FormLog.innerHTML = `
     <div class="Wrapper-form-login">
       <div class="Container-Header">
         <h1>LOGIN</h1>
@@ -27,12 +27,12 @@ function createLoginForm() {
         <button class="button"type="submit">SIGN IN</button>
       </div>
       <div class="Option-account">
-        <p><strong>Don't have an account? </strong><a href="#">Create an account</a></p>
+        <p><strong>Don't have an account? </strong><a href="/pages/Registration.html">Create an account</a></p>
         <p><a href="#">Forgot your password?</a></p>
       </div>
     </div>
   `;
-  return form;
+  return FormLog;
 }
 // ------------------------------//
 //  END From Login Structure     //
---
+++ JS/FormRegistration.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const FormRegistration = document.getElementById("FormRegistrationJS");
+  const RegistrationForm = CreateRegistrationForm();
+  FormRegistration.appendChild(RegistrationForm);
+});
+
+function CreateRegistrationForm() {
+  const FormRegis = document.createElement("form");
+  FormRegis.className = "RegistrationForm";
+  FormRegis.innerHTML = `
+    <div class="Wrapper-form-registration">
+        <div class="Container-Header">
+            <h1>Registration</h1>
+            <p>Enter your information below to proceed. If you already have an account, please log in instead.</p>
+        </div>
+        <div class="container-input-name">
+            <input type="text" id="fist-name" name="first name" placeholder="First Name">
+            <input type="text" id="last-name" name="last name" placeholder="Last Name">
+        </div>
+        <div class="container-input-email-and-password">
+            <input type="email" name="email" placeholder="Email Address" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button class="button"type="submit">SIGN IN</button>
+        </div>
+        <div class="Option-account">
+            <p><strong>Already have an account? </strong><a href="/pages/Login.html">Login</a></p>
+        </div>
+    </div>
+    `;
+  return FormRegis;
+}
