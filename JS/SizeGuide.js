2830112a077f91a2f07a08bc6d1bf04efe764723
9Nadi_ <marrief19@gmail.com>
2024-08-15 10:48:56 +0700

    add page size guide

--- JS/SizeGuide.js
+++ JS/SizeGuide.js
@@ -12,69 +12,87 @@ function CreateSizeGuide() {
       <h1>TOMOS Size Guide</h1>
     </div>
     <div class="Container-sz-guide-tshirt">
-      <h4>Shoes Tomos Limited Edition</h4>
+      <div class="wrapper-table-sz-guide">
+        <h4>T-shirt Category</h4>
         <table>
-          <thead>
-            <tr>
-              <th>CM</th>
-              <th>US</th>
-              <th>EU</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>23</td>
-              <td>4.5</td>
-              <td>36</td>
-            </tr>
-            <tr>
-              <td>23.5</td>
-              <td>5</td>
-              <td>37</td>
-            </tr>
-            <tr>
-              <td>24</td>
-              <td>6</td>
-              <td>38</td>
-            </tr>
-            <tr>
-              <td>25</td>
-              <td>7</td>
-              <td>39</td>
-            </tr>
-            <tr>
-              <td>25.5</td>
-              <td>7.5</td>
-              <td>40</td>
-            </tr>
-            <tr>
-              <td>26</td>
-              <td>8</td>
-              <td>41</td>
-            </tr>
-            <tr>
-              <td>27</td>
-              <td>9</td>
-              <td>42</td>
-            </tr>
-            <tr>
-              <td>27.5</td>
-              <td>9.5</td>
-              <td>43</td>
-            </tr>
-            <tr>
-              <td>28</td>
-              <td>10</td>
-              <td>44</td>
-            </tr>
-            <tr>
-              <td>29</td>
-              <td>11</td>
-              <td>45</td>
-            </tr>
-          </tbody>
-        </table>
+        <thead>
+          <tr>
+            <th>Size (cm)</th>
+            <th>S</th>
+            <th>M</th>
+            <th>L</th>
+            <th>XL</th>
+            <th>XXL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Length</td>
+            <td>70</td>
+            <td>72</td>
+            <td>74</td>
+            <td>76</td>
+            <td>78</td>
+          </tr>
+          <tr>
+            <td>Hand Length</td>
+            <td>9</td>
+            <td>11</td>
+            <td>13</td>
+            <td>15</td>
+            <td>17</td>
+          </tr>
+          <tr>
+            <td>Width</td>
+            <td>24</td>
+            <td>26</td>
+            <td>28</td>
+            <td>30</td>
+            <td>32</td>
+          </tr>
+        </tbody>
+      </table>
+        <h4>Hoodie Category</h4>
+        <table>
+        <thead>
+          <tr>
+            <th>Size (cm)</th>
+            <th>S</th>
+            <th>M</th>
+            <th>L</th>
+            <th>XL</th>
+            <th>XXL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Length</td>
+            <td>70</td>
+            <td>72</td>
+            <td>74</td>
+            <td>76</td>
+            <td>78</td>
+          </tr>
+          <tr>
+            <td>Hand Length</td>
+            <td>9</td>
+            <td>11</td>
+            <td>13</td>
+            <td>15</td>
+            <td>17</td>
+          </tr>
+          <tr>
+            <td>Width</td>
+            <td>24</td>
+            <td>26</td>
+            <td>28</td>
+            <td>30</td>
+            <td>32</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
     `;
   return SzGuide;
 }
